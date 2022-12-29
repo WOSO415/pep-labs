14e11b4613ddf86afb054a830fa8b7a107ffbd2b
Matt Worthy <mattworthy01@gmail.com>
2022-12-29 18:30:53 +0000

    Saved progress on Instantiation 2022-12-29 18:30:53 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        tool calc = new tool();
+        return calc;
     }
 }
