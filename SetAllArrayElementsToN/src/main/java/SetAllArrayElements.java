11b57f4e1c45cf1571ff01ebea66554ed16a1c1a
Matt Worthy <mattworthy01@gmail.com>
2023-01-02 14:33:13 +0000

    Saved progress on SetAllArrayElementsToN 2023-01-02 14:33:13 +0000

--- SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
+++ SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
@@ -28,7 +28,7 @@ public class SetAllArrayElements {
      */
     public String set(int[] arr, int n){
         for (int i = 0; i < arr.length; i++) {
-           int val;
+           
         Arrays.fill(arr, n);
            
         }
