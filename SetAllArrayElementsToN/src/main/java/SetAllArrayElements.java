2e7f2eca5553aa5eeb3dcd37f2f1d83bd0f5a305
Matt Worthy <mattworthy01@gmail.com>
2023-01-02 14:32:10 +0000

    Saved progress on SetAllArrayElementsToN 2023-01-02 14:32:10 +0000

--- SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
+++ SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
@@ -23,13 +23,16 @@ public class SetAllArrayElements {
      *
      * @param arr an array.
      * @param n number that you will switch all numbers of arr to.
+     * @return 
      * @return nothing, because of pass-by-reference, all changes will be reflected in the original array.
      */
-    public void set(int[] arr, int n){
+    public String set(int[] arr, int n){
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] + 100;
+           int val;
+        Arrays.fill(arr, n);
+           
         }
-    }
-
+        return null;
     
+}
 }
