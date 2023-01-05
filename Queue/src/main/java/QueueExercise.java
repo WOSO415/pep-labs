ef5d19f1aa99dcbcb1e05befed2e788d4f63ac34
Matt Worthy <mattworthy01@gmail.com>
2023-01-05 22:07:13 +0000

    Saved progress on Queue 2023-01-05 22:07:13 +0000

--- Queue/src/main/java/QueueExercise.java
+++ Queue/src/main/java/QueueExercise.java
@@ -2,6 +2,7 @@
  * TODO: switch to deque
  */
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -29,7 +30,8 @@ public class QueueExercise {
      * Stack behavior.
      */
     public Queue<String> createQueue(){
-        return null;
+        Queue<String> str_queue = new LinkedList<>();
+        return str_queue;
     }
 
     /**
@@ -38,7 +40,8 @@ public class QueueExercise {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        int que = queue.size();
+        return que;
     }
 
     /**
@@ -48,7 +51,7 @@ public class QueueExercise {
      * @param item a String that should be added to Queue.
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
-
+        queue.add(item);
     }
 
     /**
@@ -58,7 +61,8 @@ public class QueueExercise {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+
+        return " ";
     }
 
     /**
