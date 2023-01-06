affcbc4a1080499b4694e05c171da8717bda7410
Matt Worthy <mattworthy01@gmail.com>
2023-01-06 17:58:29 +0000

    Saved progress on Casting 2023-01-06 17:58:29 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,7 +18,7 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        
-        return 0;
+        double a = (double) ;
+        return a;
     }
 }
