6317d69ed970e73f2261ba8f1165168ab84a821d
Matt Worthy <mattworthy01@gmail.com>
2023-01-07 08:35:31 +0000

    Saved progress on GetMax 2023-01-07 08:35:31 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -11,7 +11,7 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        int maxVal = -901;
+        int maxVal = arr[0];
         
         for (int i = 0; i < arr.length; i++) {
             if (maxVal < arr[i]) {
