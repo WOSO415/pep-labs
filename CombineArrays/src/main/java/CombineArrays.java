1b4f0146610cceaf4fde57aef042c7e60d02af74
Matt Worthy <mattworthy01@gmail.com>
2023-01-27 17:44:32 +0000

    Saved progress on CombineArrays 2023-01-27 17:44:32 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class CombineArrays {
     /**
@@ -8,7 +10,15 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+       int aLen = arr1.length;
+       int bLen = arr2.length;
+       int c = aLen + bLen;
+       int[] arr3 = new int[c];
+        System.arraycopy(arr1, 0, arr3, 0, aLen);
+        System.arraycopy(arr2, 0, arr3, aLen, bLen);
+        return arr3;
+        
 
-        return null;
     }
+
 }
