7cec226498cfb4c333ddaf45378427261c53f412
Matt Worthy <mattworthy01@gmail.com>
2023-01-29 23:43:22 +0000

    Saved progress on Library 2023-01-29 23:43:22 +0000

--- Library/src/main/java/Application/DAO/BookDAO.java
+++ Library/src/main/java/Application/DAO/BookDAO.java
@@ -88,12 +88,15 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "insert into book (title) values (?)" ;
+            String sql = "insert into book (isbn, author_id, title, copies_available) values (?, ?, ?, ?)" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             
             
             //write preparedStatement's setString and setInt methods here.
-            preparedStatement.setString(1, book.getTitle());
+            preparedStatement.setInt(1, book.getIsbn());
+            preparedStatement.setInt(2, book.getAuthor_id());
+            preparedStatement.setString(3, book.getTitle());
+            preparedStatement.setInt(4, book.getCopies_available());
             preparedStatement.executeUpdate();
             return book;
         }catch(SQLException e){
