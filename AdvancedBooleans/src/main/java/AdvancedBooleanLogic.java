f3293285070754767ea1c67e3ae76c14f6591ddb
Matt Worthy <mattworthy01@gmail.com>
2023-01-30 23:31:46 +0000

    Saved progress on AdvancedBooleans 2023-01-30 23:31:46 +0000

--- AdvancedBooleans/src/main/java/AdvancedBooleanLogic.java
+++ AdvancedBooleans/src/main/java/AdvancedBooleanLogic.java
@@ -16,12 +16,11 @@ public class AdvancedBooleanLogic {
      * @return false if b1 and b2 are both true. Otherwise, return true.
      */
     public boolean returnNotBoth(boolean b1, boolean b2){
-       if ((b1 = true) && (b2 == true)) {
-        return false;
-    } else {
-        return true;
+       if (b1 && b2) {        
+       return false;
+       }
+    return true;
     }
-}
     /**
      * b1 OR b2, BUT NOT BOTH
      * @param b1 first boolean to be compared.
@@ -29,10 +28,11 @@ public class AdvancedBooleanLogic {
      * @return true if either b1 or b2 is true, but false if they are both true or both false.
      */
     public boolean returnExclusiveOr(boolean b1, boolean b2){
-        if (b1 == true && b2 == true){
+        if (b1 != b2){
             return true;
-        }
-        return false;
+        } else {
+            return false;
+        }            
     }
 
     /**
@@ -42,6 +42,11 @@ public class AdvancedBooleanLogic {
      * @return true if neither b1 nor b2 are true. Return false if either b1 or b2 is true.
      */
     public boolean returnNeitherNor(boolean b1, boolean b2){
-        return b1 != b2;
+        if (b1 || b2) {
+        return false;
+        } else if (!(b1 || b2)); 
+        
+        
+        return true;
     }
 }
