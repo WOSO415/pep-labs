805450fb75fd214cf9a36757b149e64187cdb3cc
Matt Worthy <mattworthy01@gmail.com>
2023-02-08 22:27:06 +0000

    Saved progress on WordCount 2023-02-08 22:27:06 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,6 +5,10 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
+        if (in == null || in.isEmpty()) {
         return 0;
     }
+    String[] words = in.split("\\s+");
+    return words.length;
+} 
 }
