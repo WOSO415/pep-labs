21fe382833ee3c6164168227e56ad6e34028a9e3
Matt Worthy <mattworthy01@gmail.com>
2023-02-09 03:23:44 +0000

    Saved progress on GroceryList 2023-02-09 03:23:44 +0000

--- ArrayAbsoluteValue/src/main/java/ArrayAbsoluteValue.java
+++ ArrayAbsoluteValue/src/main/java/ArrayAbsoluteValue.java
@@ -1,5 +1,6 @@
 
 public class ArrayAbsoluteValue {
+
     /**
      * Given an array of integers 'nums', produce an array of integers composed of the absolute values of all the
      * numbers in 'nums'. Absolute value represents the distance from zero, so negative numbers should become positive
@@ -8,7 +9,15 @@ public class ArrayAbsoluteValue {
      * @param nums an array.
      * @return the absolute value array of nums.
      */
+    int[][] getAbsoluteValues;
     public int[] getArrayAbs(int[] nums){
-        return null;
+        
+
+        int[] absoluteValues = getAbsoluteValues[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                absoluteValues[i] = Math.abs(nums[i]);
+            }
+        return absoluteValues;
+     
     }
 }
